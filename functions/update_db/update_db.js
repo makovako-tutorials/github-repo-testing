c313e0fa9994223b203d4f28cde2e87962c1bdad
Marek Valko <valko.marek@gmail.com>
2020-03-21 14:34:26 +0100

    Added github functionality with authorization

--- functions/update_db/update_db.js
+++ functions/update_db/update_db.js
@@ -1,45 +1,62 @@
 const { Octokit } = require('@octokit/rest')
 
 exports.handler = async function(event, context, callback) {
-    const auth = event.headers.authorization
-    const answer = auth === process.env.EVENT_API_KEY ? 'OK' : 'NOK'
-
-    let response = {
-        statusCode: 200,
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({
-            answer
+    const {authorization} = event.headers
+    if (authorization === process.env.EVENT_API_KEY) {
+        const octokit = new Octokit({
+            auth: process.env.GITHUB_API_KEY
         })
+        const data = await octokit.repos.getContents({
+            owner: 'makovako-tutorials',
+            repo: 'repo-db-test',
+            path:'/db.json'
+        })
+        
+        let db = JSON.parse(Buffer.from(data.data.content, 'base64').toString())
+        const sha = data.data.sha
+        const payload = Math.floor(Math.random()*100)
+        db.push(payload)
+        const contents = Buffer.from(JSON.stringify(db)).toString('base64')
+        const resp = await octokit.repos.createOrUpdateFile({
+            owner: 'makovako-tutorials',
+            repo: 'repo-db-test',
+            path: 'db.json',
+            message: `Updated at ${new Date().toISOString()}`,
+            content: contents,
+            committer: {
+                name: 'Test bot app',
+                email: 'test@test.test'
+            },
+            sha
+        })
+        let response = {
+            statusCode: 200,
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                payload,
+                status: resp.status,
+                message: resp.data.commit.message
+
+            })
+        }
+        return response
+    } else {
+        let response = {
+            statusCode: 401,
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                msg: "Wrong authorization"
+            })
+        }
+        return response
     }
-    return response
-    const octokit = new Octokit({
-        auth: process.env.API_KEY
-    })
-    const data = await octokit.repos.getContents({
-        owner: 'makovako-tutorials',
-        repo: 'repo-db-test',
-        path:'/db.json'
-    })
-    console.log('Hello');
+
     
-    let db = JSON.parse(Buffer.from(data.data.content, 'base64').toString())
-    const sha = data.data.sha
-    db.push(Math.floor(Math.random()*100))
-    const contents = Buffer.from(JSON.stringify(db)).toString('base64')
-    const resp = await octokit.repos.createOrUpdateFile({
-        owner: 'makovako-tutorials',
-        repo: 'repo-db-test',
-        path: 'db.json',
-        message: `Updated at ${new Date().toISOString()}`,
-        content: contents,
-        committer: {
-            name: 'Test bot app',
-            email: 'test@test.test'
-        },
-        sha
-    })
+    
     callback(null, {
     statusCode: 200,
     body: {
