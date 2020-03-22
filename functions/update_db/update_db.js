f2f15d74902d9ebb7851a5c25bfbb5fb9501b324
Marek Valko <valko.marek@gmail.com>
2020-03-22 14:04:30 +0100

    missing await again

--- functions/update_db/update_db.js
+++ functions/update_db/update_db.js
@@ -35,7 +35,7 @@ exports.handler = async function(event, context, callback) {
         // }
         // db.push(payload)
         let response_db = []
-        const x = Promise.all(ids.map(async id => {
+        const x = await Promise.all(ids.map(async id => {
             const url = create_url(id)
             const alza_response = await fetch(url)
             const alza_data = await alza_response.text()
